bb57833c50c3e7af4bec6bdce12bfb2c7b73b544
kwstaskara <ntin_kara@hotmail.com>
2018-01-12 00:20:24 +0200

    Gulp and autoprefixer package node

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+
+var autoprefixer = require('gulp-autoprefixer');
+
+
+var input = 'scss/pyramid.scss';
+
+var output = '.css';
+gulp.task('sass', function () {
+
+gulp //add gulp without return to keep session going
+
+// Find all .scss files from the sass/ folder
+
+.src(input)
+
+// Run Sass on those files || pipe adds everything together
+
+.pipe(sass().on('error', sass.logError)) //error log to keep session going when scss contains error
+
+.pipe( autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ) )
+
+// Write the resulting CSS in the output folder
+
+.pipe(gulp.dest(output));
+
+});
+gulp.task("default", ['sass'], function() {
+
+gulp.watch(input, ['sass']);
+
+});
+
