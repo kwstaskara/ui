03ef37782dbd3d8dcbf1a40560ffe5aeec881a24
kwstaskara <ntin_kara@hotmail.com>
2018-01-12 14:52:27 +0200

    Update gulp gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var autoprefixer = require('gulp-autoprefixer');
 
 var input = 'scss/pyramid.scss';
 
-var output = '.css';
+var output = 'dist';
 gulp.task('sass', function () {
 
 gulp //add gulp without return to keep session going
@@ -20,7 +20,7 @@ gulp //add gulp without return to keep session going
 
 .pipe(sass().on('error', sass.logError)) //error log to keep session going when scss contains error
 
-.pipe( autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ) )
+.pipe( autoprefixer( 'last 20 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ) )
 
 // Write the resulting CSS in the output folder
 
