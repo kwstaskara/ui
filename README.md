7754e9a78da6af986ec2e68cb1fded6b1ebfe9db
kwstaskaragiwrgos <ntin_kara@hotmail.com>
2018-02-15 20:54:02 +0200

    Navbars updade to cssnano and autoprefixer npm yarn

--- README.md
+++ README.md
@@ -3,9 +3,13 @@
     <img src="https://ui.redpyramids.com/media/pyramidlogo.svg" alt="" width=170 height=170>
   </a>
 </p>
-  <h3 align="center">Pyramid</h3>
+  <h3 align="center">Pyramid Ui</h3>
 
+ <p align="center">
+   Pyramid Ui is a web front-end framework based in multiple css features.
+<br>
+ <a href="https://ui.redpyramids.com/docs">Documentation</a>
+    ·
+    <a href="https://blog.redpyramids.com/">Blog</a>
+</p>
 
-Pyramid is a new modern responsive lightweight and 0 dependencies innovative css framework 
-
-<h3> Under development </h3>
--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@ var gulp = require('gulp');
 
 var sass = require('gulp-sass');
 
-var cssnano = require('gulp-cssnano');
-
 var rename = require('gulp-rename');
 
-var autoprefixer = require('gulp-autoprefixer');
+var cssnano = require('cssnano');
 
+var postcss = require('gulp-postcss');
+
+var autoprefixer = require('autoprefixer');
+
+var wait = require('gulp-wait2');
 
 var input = 'scss/pyramid.scss';
 
@@ -15,26 +18,52 @@ var input2 = 'dist/pyramid.css';
 
 var output = 'dist';
 
-gulp.task('cssnano', function() {
-    return gulp.src(input2)
-    .pipe(cssnano())
-    .pipe(rename("pyramid.min.css"))
-    .pipe(gulp.dest(output));
-    
-});
+var fs = require('fs');
+
 
 gulp.task('sass', function () {
-gulp 
-.src(input)
-.pipe(sass().on('error', sass.logError)) //error log to keep session going when scss contains error
-.pipe( autoprefixer( 'last 20 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ) )
+    return gulp.src(input)
+    .pipe(sass().on('error', sass.logError)) //error log to keep session going when scss contains error
+    .pipe(gulp.dest(output));
+    
+  
+    });
+    gulp.task("default", ['sass'], function() {
+    
+    gulp.watch(input, ['sass']);
 
-.pipe(gulp.dest(output));
-gulp.start('cssnano');
+    gulp.start('check');
+
+   
+    });
+    
+
+    gulp.task('check', function () {
+
+        if (fs.existsSync(input2)) {
+            gulp.start('autoprefixer');
+            gulp.start('cssnano');
+          } else {
+            console.log('FILE DOES NOT EXIST');
+          }
+    });
+
+   
+
+
+gulp.task('autoprefixer', function () {
+    return gulp.src(input2)
+        .pipe(postcss([ autoprefixer(['> 1%', 'last 10 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11']) ]))
+        .pipe(gulp.dest(output));
 });
-gulp.task("default", ['sass'], function() {
 
-gulp.watch(input, ['sass']);
 
+gulp.task('cssnano', function () {
+    var plugins = [
+        cssnano()
+    ];
+    return gulp.src(input2)
+        .pipe(postcss(plugins))
+        .pipe(rename("pyramid.min.css"))
+        .pipe(gulp.dest(output));
 });
-
