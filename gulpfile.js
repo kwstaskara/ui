c38b6bbec3559ac885d9b6f62f18d1856512def7
kwstaskara <ntin_kara@hotmail.com>
2018-05-22 23:00:03 +0300

    Gulpfile compiles different flavors. New flavors added

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,10 @@ var input2 = 'dist/pyramid.css';
 
 var input3 = 'dist/pyramid.min.css';
 
+var input4 = 'dist/pyramid-nofallbacks.css';
+
+var input5 ='dist/pyramid-prefixed-nofallbacks.css';
+
 var output = 'dist';
 
 var fs = require('fs');
@@ -39,7 +43,16 @@ gulp.task('sass', function () {
    
     });
     
-    
+    gulp.task('sass-norem', function () {
+        return gulp.src(input)
+        .pipe(header('$rem-fallback: ' + false + '!default' ))
+        .pipe(sass().on('error', sass.logError)) //error log to keep session going when scss contains error
+        .pipe(rename("pyramid-nofallbacks.css"))
+        .pipe(gulp.dest(output));
+        
+      
+        });
+
 
     gulp.task('check', function () {
 
@@ -47,6 +60,10 @@ gulp.task('sass', function () {
             gulp.start('minify-css');
             gulp.start('autoprefixer');
             gulp.start('autoprefixer2');
+            gulp.start('autoprefixer3');
+            gulp.start('sass-norem');
+            gulp.start('minify-css2');
+            gulp.start('minify-css3');
           } else {
             console.log('FILE DOES NOT EXIST');
           }
@@ -57,6 +74,7 @@ gulp.task('sass', function () {
 
 gulp.task('autoprefixer', function () {
     return gulp.src(input2)
+        .pipe(rename("pyramid-prefixed.css"))
         .pipe(postcss([ autoprefixer(['> 1%', 'last 10 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11']) ]))
         .pipe(gulp.dest(output));
 });
@@ -65,6 +83,15 @@ gulp.task('autoprefixer', function () {
 gulp.task('autoprefixer2',['autoprefixer'], function () {
     return gulp.src(input3)
         .pipe(postcss([ autoprefixer(['> 1%', 'last 10 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11']) ]))
+        .pipe(rename("pyramid-prefixed.min.css"))
+        .pipe(gulp.dest(output));
+});
+
+
+gulp.task('autoprefixer3',['autoprefixer2'], function () {
+    return gulp.src(input4)
+        .pipe(postcss([ autoprefixer(['> 1%', 'last 10 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11']) ]))
+        .pipe(rename("pyramid-prefixed-nofallbacks.css"))
         .pipe(gulp.dest(output));
 });
 
@@ -72,5 +99,21 @@ gulp.task('minify-css', () => {
     return gulp.src(input2)
       .pipe(csso())
       .pipe(rename("pyramid.min.css"))
+      .pipe(gulp.dest(output));
+  });
+
+
+  gulp.task('minify-css2', ['minify-css'], () => {
+    return gulp.src(input4)
+      .pipe(csso())
+      .pipe(rename("pyramid-nofallbacks.min.css"))
+      .pipe(gulp.dest(output));
+  });
+
+
+  gulp.task('minify-css3', ['autoprefixer3'], () => {
+    return gulp.src(input5)
+      .pipe(csso())
+      .pipe(rename("pyramid-prefixed-nofallbacks.min.css"))
       .pipe(gulp.dest(output));
   });
